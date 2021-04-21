1808d7c4ae5a6a1757300a08b55a9c1318c95d1f
Kilian <friedrich.kilian@outlook,de>
2021-04-21 12:47:30 +0200

    feat: Inserts

--- src/main/resources/inserts.sql
+++ src/main/resources/inserts.sql
@@ -36,3 +36,42 @@ VALUES
 //Es fehlen Ernte, Essen, Essen_Anbau, Essen_Forscher
 //Forscher-Table angepassen -> Kein Abflug
 //Rakete-Table anpassen -> Kein Abflug
+
+insert into fahrzeug (name, max_passagiere, ps) values
+("Elektrischer Rasenmäher", 1, 20),
+("Ingenioutier", 4, 500),
+("Trabbi 2", 5, 100);
+select * from fahrzeug;
+
+select * form forscher;
+
+insert into expedition (startzeit, endzeit) values
+("2030-09-13T11:15:00", "2030-09-13T19:49:00"),
+("2030-09-14T11:00:00", "2030-09-21T18:32:00"),
+("2030-09-20T07:00:00", "2030-09-20T09:11:00"),
+("2030-09-24T12:30:00", "2030-12-13T12:00:00"),
+("2030-10-13T11:15:00", "2030-10-14T19:49:00");
+select * from expedition;
+
+insert into logbuch_eintrag (zeit, inhalt, expedition_fk) values
+("2030-09-13T18:00", "Toller Tag, tolle Aussicht, 10/10 would recommend - aber jetzt erstmal zurück fahren und essen", 1),
+("2030-09-14T12:00", "JETZT GEHTS LOOOOOS JETZT GEHTS LOOOOOS JAAAAY", 2),
+("2030-09-18T18:00", "Heute habe ich ein Alien gesehen :O", 2),
+("2030-09-21T12:00", "Richtig mieses Wetter, schwach", 2),
+("2030-09-20T07:00", "Pizza + Ananas = <3", 3),
+("2030-09-20T08:00", "Ding Dong der letzte Logbucheintrag war wrong", 3),
+("2030-09-20T19:00", "Das war ein toller Ausflug", 3),
+("2030-09-13T18:00", "Ich schreibe doch kein Logbuch hä", 4),
+("2030-09-13T18:00", "Seh ich aus wie Göthe oder was?", 4),
+("2030-10-13T12:00", "Wir haben draußen ein Solarpanel repariert", 5);
+select * from logbuch_eintrag;
+
+insert into forscher_auf_expedition (expedition_fk, forscher_fk, fahrzeug_fk) values
+(1, 4, 4),
+(1, 1, 4),
+(1, 2, 4),
+(1, 5, 4),
+(2, ),
+(3, ),
+(4, ),
+(5, );
